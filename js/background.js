5139bc0f8198d9adab09182a9c67e761f54f7afe
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2016-05-15 00:18:19 +0200

    fix creating context menu on selection

--- js/background.js
+++ js/background.js
@@ -1,3 +1,5 @@
+/* Keyboard Shortcuts */
+
 var actions = {
   'search-word': 'js/definition.js'
 };
@@ -18,3 +20,36 @@ chrome.commands.onCommand.addListener(function (command) {
     performCommand(activeTab, command);
   });
 });
+
+/* Context Menu */
+
+chrome.contextMenus.create({"id": "parent", "title": "Look up word", "contexts": ["selection"]});
+chrome.contextMenus.create({"id": "first-child", "title": "Definition", "contexts": ["selection"], "parentId": "parent"});
+chrome.contextMenus.create({"id": "second-child", "title": "Word history", "contexts": ["selection"], "parentId": "parent"});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  switch(info.menuItemId) {
+    case 'first-child':
+      searchDefinition();
+      break;
+    case 'second-child':
+      showHistory();
+      break;
+    default:
+      console.log('Invalid option');
+  }
+});
+
+function searchDefinition () {
+  chrome.tabs.executeScript( {
+      code: "window.getSelection().toString();"
+    }, function(selection) {
+      var word = selection[0];
+      //llamar a la api para que devuelva la definicion
+
+  });
+}
+
+function showHistory () {
+
+}
