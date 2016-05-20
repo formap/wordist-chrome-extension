6169855664ab6eba706be4c0607c5a14550f192a
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2016-05-20 16:58:53 +0200

    save login token in local storage

--- js/login.js
+++ js/login.js
@@ -12,17 +12,35 @@ function getCurrentTabUrl(callback) {
   });
 }
 
-function validateData(email, password) {
-  if (!email || !password) return false;
+function loginUser(userData) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      var response = JSON.parse(xhr.responseText);
+      localStorage.setItem('id', response.user._id);
+      localStorage.setItem('token', response.token);
+    }
+  }
+  var url = 'https://wordist-backend.herokuapp.com/auth/login';
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(userData);
+}
+
+function validateData(userData) {
+  if (!userData.email || !userData.password) return false;
   var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailRegex.test(email);
+  return emailRegex.test(userData.email);
 }
 
 function addSigninListener() {
   document.getElementById('sign-in-button').addEventListener('click', function (e) {
-    var email = document.getElementById('sign-in-email').value;
-    var password = document.getElementById('sign-in-password').value;
-    var valid = validateData(email, password);
+    var userData = {
+      email: document.getElementById('sign-in-email').value,
+      password: document.getElementById('sign-in-password').value
+    }
+    var valid = validateData(userData);
+    if (valid) loginUser(JSON.stringify(userData));
   });
 }
 
